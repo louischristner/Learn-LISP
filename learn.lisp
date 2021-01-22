38fb58dec34b529a8fd471dfcb1bcf04c04cdb7f
Louis Christner <louis.christner@epitech.eu>
2021-01-22 12:19:44 +0100

    Create iterative and recursive fizzbuzz

--- learn.lisp
+++ learn.lisp
@@ -3,4 +3,31 @@
         (+ x (factorial (- x 1)))
         1))
 
-(write (factorial 3))
+(defun display-fizz-buzz(start end)
+    (setq line "")
+    (if (= (mod start 3) 0)
+        (setq line "fizz"))
+    (if (= (mod start 5) 0)
+        (setq line (concatenate 'string line "buzz")))
+    (if (string-not-equal line "")
+        (write-line line))
+)
+
+(defun fizzbuzz-recursive(start end incr)
+    (block b-rec
+        (if (> start end)
+            (return-from b-rec '()))
+        (display-fizz-buzz start end)
+        (fizzbuzz-recursive (+ start incr) end incr)
+    )
+)
+
+(defun fizzbuzz-iterative(start end incr)
+    (loop
+        (display-fizz-buzz start end)
+        (setq start (+ start incr))
+        (when (> start end) (return start))
+    )
+)
+
+(fizzbuzz-iterative 0 100 1)
